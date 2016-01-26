165c303c253b08584f52a56e06a7d2f1f0f79ebc
Ruti Wajnberg <ruti.wajnberg@smashingboxes.com>
2016-01-26 10:09:59 -0500

    Add test for post create controller action.

--- spec/controllers/to_dos_controller_spec.rb
+++ spec/controllers/to_dos_controller_spec.rb
@@ -4,12 +4,26 @@ RSpec.describe ToDosController, type: :controller do
 
     let(:to_do) {FactoryGirl.create(:to_do)}
 
-    describe "GET index" do
-      it "returns all todos in json format" do
+    describe "GET /todos.json" do
+      before do
         get :index
+      end
+      it "returns all todos in json format" do
+        expect(response.content_type).to eq "application/json"
         expect(response).to include
-          ({ "title": "I have to do this thang",
-             "is_completed": true })
+          ({
+            "title": "I have to do this thang",
+            "is_completed": true
+            })
+      end
+    end
+
+    describe "POST /todos.json" do
+      before do
+        post :create, to_do: ({title: "New title", is_completed: true})
+      end
+      it "creates a new post" do
+        expect(ToDo.last.title).to eq "New title"
       end
     end
 end
